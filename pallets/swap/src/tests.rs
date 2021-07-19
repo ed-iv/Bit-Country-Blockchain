e6775e2efab7ba2f57bf001fcd90cd6d319193f0
Ed Henriques IV <ehenriquesiv@gmail.com>
2021-07-18 21:40:18 -0700

    Write tests - WIP

--- pallets/swap/src/tests.rs
+++ pallets/swap/src/tests.rs
@@ -23,10 +23,103 @@ use mock::{Event, *};
 use sp_core::blake2_256;
 use sp_runtime::traits::BadOrigin;
 
+fn check_balances(
+    balances: Vec<(&AccountId, SocialTokenCurrencyId, mock::Balance)>) -> bool {
+    for (who, currency_id, balance) in balances {        
+        assert_eq!(SocialCurrencies::total_balance(currency_id, &who), balance);        
+    }
+    true
+}
+
 #[test]
-fn mock_works() {
+fn add_liquidity_should_not_work() {
     ExtBuilder::default().build().execute_with(|| {        
-        assert_eq!(1, 1);
+        // As written code requires pair begins w/ native token (NUUM)
+        assert_noop!(
+            SwapModule::add_liquidity(ALICE.into(), SOC_A, SOC_B, 10, 10),
+            Error::<Runtime>::InvalidSocialTokenIds
+        );
+        // NUUM balance too low
+        assert_noop!(
+            SwapModule::add_liquidity(ALICE.into(), NUUM, SOC_A, 1000, 10),
+            pallet_balances::Error::<Runtime>::InsufficientBalance
+        );
+        // SOC_A balance too low
+        assert_noop!(
+            SwapModule::add_liquidity(ALICE.into(), NUUM, SOC_A, 10, 1000),
+            orml_tokens::Error::<Runtime>::BalanceTooLow
+        );
+        // Below existential deposit
+        assert_noop!(
+            SwapModule::add_liquidity(ALICE.into(), NUUM, SOC_A, 100, 10),
+            pallet_balances::Error::<Runtime>::KeepAlive
+        );
+        // Invalid Liquidity Increment
+        assert_noop!(
+            SwapModule::add_liquidity(ALICE.into(), NUUM, SOC_A, 0, 10),
+            Error::<Runtime>::InvalidLiquidityIncrement
+        );
+        assert_noop!(
+            SwapModule::add_liquidity(ALICE.into(), NUUM, SOC_A, 10, 0),
+            Error::<Runtime>::InvalidLiquidityIncrement
+        );
+        assert_noop!(
+            SwapModule::add_liquidity(ALICE.into(), NUUM, SOC_A, 0, 0),
+            Error::<Runtime>::InvalidLiquidityIncrement
+        );
+
+        // Pair not enabled
+        // let status = TradingPairStatus::NotEnabled; 
+        // let pair = TradingPair::from_token_currency_ids(NUUM, SOC_A);
+        // TradingPairStatuses::<Runtime>::insert(pair, status);
+        // assert_noop!(
+        //     SwapModule::add_liquidity(ALICE.into(), NUUM, SOC_A, 10, 10),
+        //     Error::<Runtime>::NotEnabledTradingPair
+        // );
     });
 }
 
+#[test]
+fn add_liquidity_should_work() {
+    ExtBuilder::default().build().execute_with(|| {    
+        let share_currency_id: SocialTokenCurrencyId = SocialTokenCurrencyId::DEXShare(0, 1);
+        let dex_account_id: AccountId = SwapModule::account_id();
+        let pair = TradingPair (NUUM, SOC_A);
+
+        assert_ok!(SwapModule::add_liquidity(ALICE.into(), pair.0, pair.1, 10, 5));
+    
+        check_balances(vec![
+            (&ALICE, pair.0, 90), 
+            (&ALICE, pair.1, 95),
+            (&ALICE, share_currency_id, 20),
+            (&dex_account_id, pair.0, 10),
+            (&dex_account_id, pair.1, 5),
+        ]);        
+    
+        assert_eq!(SwapModule::liquidity_pool(pair), (10, 5));
+        let event = mock::Event::swap(crate::Event::AddLiquidity(ALICE, pair.0, 10, pair.1, 5, 20));
+        assert_eq!(last_event(), event);    
+    });
+}
+
+#[test]
+fn add_liquidity_should_work_with_less_nuum() {
+    ExtBuilder::default().build().execute_with(|| {    
+        assert_ok!(SwapModule::add_liquidity(ALICE.into(), NUUM, SOC_A, 5, 10));
+
+        let share_currency_id: SocialTokenCurrencyId = SocialTokenCurrencyId::DEXShare(0, 1);
+        let dex_account_id: AccountId = SwapModule::account_id();
+        
+        // Check ALICE's balances
+        assert_eq!(SocialCurrencies::total_balance(NUUM, &ALICE), 95);
+        assert_eq!(SocialCurrencies::total_balance(SOC_A, &ALICE), 90);
+        assert_eq!(SocialCurrencies::total_balance(share_currency_id, &ALICE), 20);
+        
+        // Check pool balances:
+        assert_eq!(SocialCurrencies::total_balance(NUUM, &dex_account_id), 5);
+        assert_eq!(SocialCurrencies::total_balance(SOC_A, &dex_account_id), 10);
+        
+        let event = mock::Event::swap(crate::Event::AddLiquidity(ALICE, NUUM, 5, SOC_A, 10, 20));
+        assert_eq!(last_event(), event);
+    });
+}
